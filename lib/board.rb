a80564fb6e48874cb35369161f6b892ffcb9c355
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-11-15 22:35:22 -0500

    Add #find_possible_moves method to Piece class

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,18 @@
-class Board
+# frozen_string_literal: true
 
+require 'observer'
+
+# contains logic for chess board
+class Board
+  include Observable
+  attr_reader :black_king, :white_king, :mode
+  attr_accessor :data, :active_piece, :previous_piece
+
+  def initialize(data = Array.new(8) { Array.new(8) }, params = {})
+    @data = data
+    @active_piece = params[:active_piece]
+    @previous_piece = params[:previous_piece]
+    @black_king = params[:black_king]
+    @white_king = params[:white_king]
+  end
 end
