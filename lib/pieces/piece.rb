5698e13335e233478c3399bb5d2f19e71c503eef
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-11-16 23:01:29 -0500

    Implemented #find_possible_captures method in Piece class
    This commit introduces the #find_possible_captures method within the
    Piece class. Currently, it identifies all potential capture moves
    available for a given chess piece on the board, without checking for
    legality. The implementation of legality checks for captures will be
    incorporated later in the remove_illegal_moves method.

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -1,16 +1,24 @@
+require_relative '../board'
+require_relative '../move_validator'
+
+# Superclass that contains movment logic for all chess peices
 class Piece
   attr_reader :color, :location, :symbol, :moves, :captures, :moved, :board
 
   def initialize(board, args)
-    @board = board
+    # These variables are initialized via passed parameters and subclass initilization method.
+    board.add_observer(self)
     @color = args[:color]
     @location = args[:location]
     @symbol = nil
-    @move_list = nil
-    @capture_list = nil
+
+    # These variables are set by movment functions
+    @moves = nil
+    @captures = nil
     @moved = false
   end
 
+  # Update the current location of the game piece.
   def update_location(rank, file)
     @location = [rank, file]
     @moved = true
@@ -19,39 +27,111 @@ class Piece
   # Returns array of valid move locations for pieces that can move multiple squares.
   def current_moves(board)
     # Create a list of possible moves
-    @move_list = find_possible_moves(board)
+    possible_moves = find_possible_moves(board)
+
+    # Remove illegal moves from possible moves list
+    @moves = remove_illegal_moves(board, possible_moves)
+  end
+
+  # Creates and returns list of all possible moves
+  def find_possible_moves(board)
+    # Iterate over move set and create list of moves
+    moves = move_set.inject([]) do |list, move|
+      list << create_moves(board.data, move)
+    end
+
+    # Return list of moves
+    moves.compact.flatten(1)
+  end
+
+  # Returns array of valid capture locations for pieces that can move multiple squares
+  def current_captures(board)
+    # Create list of possible moves/captures
+    possible_captures = find_possible_captures(board)
+
+    # Remove illegal moves/captures
+    @captures = remove_illegal_moves(board, possible_captures)
+  end
+
+  # Returns list of possible captures 
+  def find_possible_captures(board)
+    # Iterate over move set and create list of moves
+    captures = move_set.inject([]) do |list, move|
+      list << create_captures(board.data, move)
+    end
+
+    # Return list of moves
+    captures.compact
+  end
+
+  # method used when notified of a change in Board (as an observer)
+  def update(board)
+    current_captures(board)
+    current_moves(board)
   end
 
   private
 
-  # Creates and returns list of all possible moves
-  def find_possible_moves(board, start = @location)
-    # Iterate over list of move transformations
-    transformations.each_with_object([]) do |transformation, list|
-      # Preform intial transformation
-      pos = update_pos(transformation, start)
+  # Returns list of transformations from sublcass
+  def move_set
+    raise 'Called abstract method: move_set'
+  end
 
-      # Repeate transformation until edge of board is reached or occupied pos is reached
-      while valid_move?(pos) && board[pos.first][pos.last].nil?
-        # Store move to list
-        list << pos
+  def create_moves(data, transformation)
+    # Preform intial transformation
+    pos = move_piece(transformation, location)
 
-        # Preform transformation
-        pos = update_pos(transformation, pos)
-      end
+    # List of moves
+    list = []
+
+    # Repeate transformation until edge of board is reached or occupied pos is reached
+    while valid_move?(pos, data) && data[pos.first][pos.last].nil?
+      # Store move to list
+      list << pos
+
+      # Preform transformation
+      pos = move_piece(transformation, pos)
     end
+
+    # Return list
+    list
+  end
+
+  # add capture to list when opposing piece is reached, based on each piece's move_set
+  def create_captures(data, transformation)
+    # Preform intial transformation
+    pos = move_piece(transformation, location)
+
+    # Move through the board
+    while valid_move?(pos, data)
+      # Stop if space is occupied
+      break if data[pos.first][pos.last]
+
+      # Move onto next location on the board
+      pos = move_piece(transformation, pos)
+    end
+
+    # Return location if it is occupied by enemy piece.
+    pos if opposing_piece?(pos, data)
+  end
+
+  # Checks if space is occupied enemy piece
+  def opposing_piece?(pos, data)
+    return unless valid_move?(pos, data)
+
+    piece = data[pos.first][pos.last]
+    piece && piece.color != color
   end
 
   # Preform transformation
-  def update_pos(transformation, pos)
+  def move_piece(transformation, pos)
     [transformation.first + pos.first, transformation.last + pos.last]
   end
 
   # Check if move is valid
-  def valid_move?(pos)
+  def valid_move?(pos, data)
     # Move is valid if rank is between 0-7
     # Move is valid if file is between 0-7
-    pos.first.between?(0, board.size - 1) && pos.last.between?(0, board.size - 1)
+    pos.first.between?(0, data.size - 1) && pos.last.between?(0, data.size - 1)
   end
 end
-
