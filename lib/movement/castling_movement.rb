02c72fc46d54c4dba5291ad1a1cae82563625f65
Alfredo Ormeno <ormenoandres13@gmail.com>
2024-01-28 01:13:52 -0500

    feat: Add CastlingMovement class
    This commit introduces the CastlingMovement class, which will handle
    the logic for castling in chess.

---
+++ lib/movement/castling_movement.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require_relative 'basic_movement'
+
+# Logic for castle moves
+class CastlingMovement < BasicMovement
+  def initialize(board = nil, pos = nil)
+    super
+  end
+end
