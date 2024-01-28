ebd76504cdf11c8718556ae0011f28913fab9bab
Alfredo Ormeno <ormenoandres13@gmail.com>
2024-01-28 01:54:24 -0500

    feat: Implement #find_castling_rook and #update_rook_position methods
    This commit implements the #find_castling_rook method in the
    CastlingMovement class. The method is responsible for
    determining the rook that is eligible for castling based on the
    king's move.
    
    Additionally, the commit introduces the #update_rook_position method,
    which updates the position of the rook on the chess board after
    castling.

--- lib/movement/castling_movement.rb
+++ lib/movement/castling_movement.rb
@@ -6,4 +6,36 @@ class CastlingMovement < BasicMovement
   def initialize(board = nil, pos = nil)
     super
   end
+
+  def update_castling_moves
+    update_new_position
+    remove_original_piece
+    update_active_piece_location
+  end
+
+  def find_castling_rook
+    row = board.active_piece.location.first
+    column = old_rook_column
+    @board.data[row][column]
+  end
+
+  def update_rook_position(rook)
+    @board.data[pos.first][new_rook_column] = rook
+  end
+
+  private
+
+  # Determines the rooks orginal location based on the column of the king's move
+  def old_rook_column
+    king_side = 7
+    queen_side = 0
+    pos.last == 6 ? king_side : queen_side
+  end
+
+  # Determines the rooks new location based on the column of the king's move
+  def new_rook_column
+    king_side = 5
+    queen_side = 3
+    pos.last == 6 ? king_side : queen_side
+  end
 end
